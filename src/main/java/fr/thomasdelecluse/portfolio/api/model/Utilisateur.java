b931ba0739a1156c1936f9e39fc74de25c3f987f
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2022-12-22 14:08:57 +0100

    Create Models and Repos

---
+++ src/main/java/fr/thomasdelecluse/portfolio/api/model/Utilisateur.java
@@ -0,0 +1,39 @@
+package fr.thomasdelecluse.portfolio.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String mdp;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+}
