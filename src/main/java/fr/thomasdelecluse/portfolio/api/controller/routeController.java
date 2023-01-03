a729b42e6137f0131c5b24a89533eb6271f03cd1
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-01-03 22:22:50 +0100

    feat: check login

--- src/main/java/fr/thomasdelecluse/portfolio/api/controller/routeController.java
+++ src/main/java/fr/thomasdelecluse/portfolio/api/controller/routeController.java
@@ -1,21 +1,24 @@
 package fr.thomasdelecluse.portfolio.api.controller;
 
+import fr.thomasdelecluse.portfolio.api.dto.TokenDTO;
 import fr.thomasdelecluse.portfolio.api.repository.UtilisateurRepository;
 import fr.thomasdelecluse.portfolio.api.model.Utilisateur;
+import fr.thomasdelecluse.portfolio.api.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.Date;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.StreamSupport;
 
 @RestController
+@CrossOrigin
 public class routeController {
 
     @Autowired
     private UtilisateurRepository utilisateurRepository;
-
-
+    @Autowired
+    TokenService generator;
     @GetMapping("/")
     List<Utilisateur> getAll() {
         return StreamSupport.stream(utilisateurRepository.findAll().spliterator(), false).toList();
@@ -26,7 +29,27 @@ public class routeController {
         utilisateurRepository.deleteById(id);
     }
 
-    @PostMapping("/add")
+    @PostMapping("/login")
+    Object newlogin(@RequestBody Utilisateur newUtilisateur) {
+        String mdp = newUtilisateur.getMdp();
+        String email = newUtilisateur.getEmail();
+
+        Iterable<Utilisateur> db = utilisateurRepository.findAll();
+        for (Utilisateur utilisateur : db) {
+            if (utilisateur.getEmail().equals(email) && utilisateur.getMdp().equals(mdp))
+            {
+                String token = generator.nextToken();
+                utilisateur.setToken(token);
+                Date date = new Date();
+                utilisateur.setCreation_date(date);
+                utilisateurRepository.save(utilisateur);
+                return new TokenDTO(token);
+            }
+        }
+        return "aucun utilisateur trouvé";
+    }
+
+    @PostMapping("/register")
     Utilisateur newUtilisateur(@RequestBody Utilisateur newUtilisateur) {
         return utilisateurRepository.save(newUtilisateur);
     }
